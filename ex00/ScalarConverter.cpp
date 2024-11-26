5b680aa3302f4c1acbd6627b03dba9c0dc70d901
Clement Deville <cdeville@made-f0Br2s4.clusters.42paris.fr>
2024-11-26 19:46:50 +0100

    First commit

---
+++ ex00/ScalarConverter.cpp
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cdeville <cdeville@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024-11-26 17:27:11 by cdeville          #+#    #+#             */
+/*   Updated: 2024-11-26 17:27:11 by cdeville         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScalarConverter.hpp"
+
+void ScalarConverter::convert(const std::string param)
+{
+    std::cout << "char: " << (int)param << std::endl;
+}
---
+++ ex00/ScalarConverter.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cdeville <cdeville@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024-11-26 17:24:19 by cdeville          #+#    #+#             */
+/*   Updated: 2024-11-26 17:24:19 by cdeville         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERTER_HPP
+# define SCALARCONVERTER_HPP
+
+# include <string> 
+# include <iostream>
+
+class ScalarConverter
+{
+public:
+    static void convert(const std::string param);
+};
+
+#endif
