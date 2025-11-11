9175f50defd82c323fa28c90314ef409fec19791
Clement-Deville <clementdeville3@gmail.com>
2025-11-11 15:08:07 +0100

    Adding main ptr test on ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cdeville <cdeville@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 16:33:43 by cdeville          #+#    #+#             */
-/*   Updated: 2024/11/29 16:49:17 by cdeville         ###   ########.fr       */
+/*   Updated: 2025/11/11 15:06:38 by cdeville         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,17 @@ int	main(void)
 {
 	Data tab[4] = {{"Clement", 1}, {"John", 2}, {"Alex", 3}, {"Michael", 4}};
 	uintptr_t ser_tab[4];
+	Data	*ptr;
 
 	for (int i = 0; i < 4; i++)
 	{
 		ser_tab[i] = Serializer::serialize(&tab[i]);
 		std::cout << "Data pointer before serialize:   \e[0;32m" << &tab[i] <<
 			"\e[0m serialize value: " << ser_tab[i] << std::endl;
+		ptr = Serializer::deserialize(ser_tab[i]);
 		std::cout << "Pointer value after deserialize: \e[0;33m"
-			<< Serializer::deserialize(ser_tab[i]) << "\e[0m" << std::endl;
+			<< ptr << "\e[0m" << std::endl;
+		std::cout << "Accessing data throught ptr: \nname: " << ptr->name << std::endl;
 	}
 	return (0);
 }
